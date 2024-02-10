ad767260719bac998168594fe917a7a661a59196
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-02-10 20:05:19 +0600

    data receive from client side with input

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.port || 5000;
+app.use(cors());
+app.use(express.json());
+
+const users = [
+  { id: 1, name: "sabana", email: "sabana@gmail.com" },
+  { id: 2, name: "Rakib", email: "rakib@gmail.com" },
+  { id: 3, name: "saab", email: "saba@gmail.com" },
+  { id: 4, name: "sabanur", email: "anurana@gmail.com" },
+];
 
 app.get("/", (req, res) => {
   res.send("hello from user server");
 });
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("server side hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
 app.listen(port, () => {
   console.log(`server is running from: ${port}`);
 });
